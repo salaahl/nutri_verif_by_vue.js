3733104c47d9eb15ca5ccfb8e98f96581744a624
salaahl <sokhona.salaha@gmail.com>
2024-06-13 00:46:34 +0200

    first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,14 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to: any, from: any, savedPosition: any) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 export default router
