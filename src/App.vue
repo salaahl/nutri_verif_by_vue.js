9b018b0beef92a8032f660a98f26bc5371a715b6
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-06-12 16:00:00 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -88,7 +88,7 @@ onMounted(() => {
             type="text"
             id="search-input"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
-            placeholder="Rechercher selon le nom du produit, la marque, une catégorie, une origine ou un code-barres"
+            placeholder="Rechercher selon le nom du produit, la marque ou une catégorie"
             required
           />
         </div>
