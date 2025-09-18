0c8d2d23efda6b29da03208c4ce47703e0a4f77c
salaahl <sokhona.salaha@gmail.com>
2025-09-18 11:30:43 +0200

    ajustement

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -46,16 +46,18 @@ onMounted(async () => {
   websiteName.classList.add('opacity-0')
 
   // Affichage conditionnel de la vidéo Youtube
-  const videoContainer = document.getElementById("video-container") as HTMLElement
-  const cookieAuthorisation = document.getElementById("accept-cookies") as HTMLElement
-  
-  cookieAuthorisation.addEventListener("click", function () {
-    if(!confirm(
-      "Cette vidéo est hébergée par YouTube. Son affichage sur ce site implique le dépôt de cookies par YouTube (Google).\n\nCes cookies sont uniquement liés à la lecture de la vidéo et n’ont pas d’effet sur vos autres services Google.\n\nVoulez-vous les accepter et afficher la vidéo ?"
-    )) {
-      return;
+  const videoContainer = document.getElementById('video-container') as HTMLElement
+  const cookieAuthorisation = document.getElementById('accept-cookies') as HTMLElement
+
+  cookieAuthorisation.addEventListener('click', function () {
+    if (
+      !confirm(
+        'Cette vidéo est hébergée par YouTube. Son affichage sur ce site implique le dépôt de cookies par YouTube (Google).\n\nCes cookies sont uniquement liés à la lecture de la vidéo et n’ont pas d’effet sur vos autres services Google.\n\nVoulez-vous les accepter et afficher la vidéo ?'
+      )
+    ) {
+      return
     }
-    localStorage.setItem("cookiesAccepted", "true");
+    localStorage.setItem('cookiesAccepted', 'true')
 
     // Remplacement du bouton par l'iframe YouTube
     videoContainer.innerHTML = `
@@ -66,11 +68,11 @@ onMounted(async () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
-    `;
-  });
+    `
+  })
 
   // Vérification si l'utilisateur a déjà accepté
-  if (localStorage.getItem("cookiesAccepted") === "true") {
+  if (localStorage.getItem('cookiesAccepted') === 'true') {
     videoContainer.innerHTML = `
       <iframe
         src="https://www.youtube.com/embed/D1jzT02IBRA"
@@ -79,7 +81,7 @@ onMounted(async () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
-    `;
+    `
   }
 
   // Searchbar
@@ -188,11 +190,12 @@ onUnmounted(() => {
     <article class="mb-8">
       <div id="video-container" class="w-full aspect-[2/1] border-[3px] rounded-xl overflow-hidden">
         <div class="cookies h-full w-full flex items-center justify-center">
-          <button 
-            id="accept-cookies"
-          >
+          <button id="accept-cookies">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="h-[75px] w-[75px]">
-              <path fill="whitesmoke" d="M187.2 100.9C174.8 94.1 159.8 94.4 147.6 101.6C135.4 108.8 128 121.9 128 136L128 504C128 518.1 135.5 531.2 147.6 538.4C159.7 545.6 174.8 545.9 187.2 539.1L523.2 355.1C536 348.1 544 334.6 544 320C544 305.4 536 291.9 523.2 284.9L187.2 100.9z"/>
+              <path
+                fill="whitesmoke"
+                d="M187.2 100.9C174.8 94.1 159.8 94.4 147.6 101.6C135.4 108.8 128 121.9 128 136L128 504C128 518.1 135.5 531.2 147.6 538.4C159.7 545.6 174.8 545.9 187.2 539.1L523.2 355.1C536 348.1 544 334.6 544 320C544 305.4 536 291.9 523.2 284.9L187.2 100.9z"
+              />
             </svg>
           </button>
         </div>
@@ -503,6 +506,8 @@ h2::first-letter {
 
 .cookies {
   background-image: url(https://i.ytimg.com/vi/D1jzT02IBRA/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgWShUMA8=&rs=AOn4CLC8xWb_RD5Ml98u-0_FNgJbU1WlHQ);
+  background-size: 105% auto;
+  background-position: 5% 0%;
 }
 
 .text-highlighted {
