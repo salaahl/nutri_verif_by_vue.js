683d5dd5d9dbe6ced08a4d0c96aa023e71f38762
salaahl <sokhona.salaha@gmail.com>
2026-05-17 00:07:18 +0200

    optimisation mineure

--- src/components/ProductScanner.vue
+++ src/components/ProductScanner.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount, ref } from 'vue'
+import { onBeforeUnmount, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { Html5Qrcode } from 'html5-qrcode'
 import { useProducts } from '../composables/useProducts'
@@ -9,36 +9,38 @@ const route = useRoute()
 
 const scannedCode = ref<string | null>(null)
 let html5QrcodeScanner: Html5Qrcode | null = null
-const cameraPermissionDenied = ref(false)
+const cameraPermissionDenied: any = ref(null)
 
 const { productIsLoading } = useProducts()
+const config = {
+  fps: 10, // Images par seconde à analyser
+  qrbox: (width: number, height: number) => {
+    const size = Math.min(width, height) * 0.8
+    return { width: size, height: size / 2 } // Format rectangulaire adapté aux codes-barres
+  }
+}
 
-onMounted(async () => {
+async function startScanner() {
   html5QrcodeScanner = new Html5Qrcode('scanner')
 
-  const config = {
-    fps: 10, // Images par seconde à analyser
-    qrbox: (width: number, height: number) => {
-      const size = Math.min(width, height) * 0.8
-      return { width: size, height: size / 2 } // Format rectangulaire adapté aux codes-barres
+  if (html5QrcodeScanner && !html5QrcodeScanner.isScanning) {
+    try {
+      await html5QrcodeScanner.start(
+        { facingMode: 'environment' }, // Caméra arrière du smartphone
+        config,
+        onScanSuccess,
+        onScanFailure
+      )
+      cameraPermissionDenied.value = false
+    } catch (err: any) {
+      console.error("Impossible d'accéder à la caméra :", err)
+      // Détection si l'erreur vient d'un refus de l'utilisateur
+      if (err.toString().includes('NotAllowedError') || err.toString().includes('Permission')) {
+        cameraPermissionDenied.value = true
+      }
     }
   }
-
-  try {
-    await html5QrcodeScanner.start(
-      { facingMode: 'environment' }, // Caméra arrière du smartphone
-      config,
-      onScanSuccess,
-      onScanFailure
-    )
-  } catch (err: any) {
-    console.error("Impossible d'accéder à la caméra :", err)
-    // Détection si l'erreur vient d'un refus de l'utilisateur
-    if (err.toString().includes('NotAllowedError') || err.toString().includes('Permission')) {
-      cameraPermissionDenied.value = true
-    }
-  }
-})
+}
 
 async function onScanSuccess(decodedText: string, decodedResult: any) {
   scannedCode.value = decodedText
@@ -77,26 +79,40 @@ onBeforeUnmount(async () => {
 </script>
 
 <template>
-  <div class="flex flex-col items-center">
+  <div class="w-full flex flex-col items-center">
     <h2 class="title mb-12 text-2xl lg:text-3xl">
       Scanner un <span class="text-[indianred]">code-barres</span>
     </h2>
     <div
-      v-if="cameraPermissionDenied"
-      class="w-full mb-12 aspect-square border-4 border-red-500 rounded-lg bg-black flex flex-col justify-center items-center p-6 text-center"
-    >
-      <p class="text-red-400 font-bold mb-2">Accès à la caméra refusé</p>
-      <p class="text-sm text-gray-300">
-        Pour scanner un produit, veuillez cliquer sur l'icône de cadenas dans la barre d'adresse de
-        votre navigateur et autoriser la caméra, puis actualisez la page.
-      </p>
-    </div>
-    <div
-      v-else
       id="scanner"
       class="h-auto w-full mb-12 aspect-square border-4 border-white rounded-lg overflow-hidden bg-black relative"
     >
       <!-- Le flux vidéo de la caméra s'affichera ici -->
+      <div
+        v-if="cameraPermissionDenied === null"
+        class="w-full mb-12 aspect-square border-4 border-red-500 rounded-lg bg-black flex flex-col justify-center items-center p-6 text-center"
+      >
+        <p class="text-red-400 font-bold mb-2">Autoriser l'accès à la caméra</p>
+        <p class="text-sm text-gray-300">
+          Pour scanner un produit, veuillez autoriser l'accès à la caméra.
+        </p>
+        <button
+          class="mt-6 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center"
+          @click="startScanner"
+        >
+          Autoriser l'accès à la caméra
+        </button>
+      </div>
+      <div
+        v-else-if="cameraPermissionDenied"
+        class="w-full mb-12 aspect-square border-4 border-red-500 rounded-lg bg-black flex flex-col justify-center items-center p-6 text-center"
+      >
+        <p class="text-red-400 font-bold mb-2">Accès à la caméra refusé</p>
+        <p class="text-sm text-gray-300">
+          Pour scanner un produit, veuillez cliquer sur l'icône de cadenas dans la barre d'adresse
+          de votre navigateur et autoriser la caméra, puis actualisez la page.
+        </p>
+      </div>
     </div>
     <div
       v-if="productIsLoading"
