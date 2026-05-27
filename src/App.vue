9ed352298cb9f5f871869d8dfe94c9f4ac97bdcb
salaahl <sokhona.salaha@gmail.com>
2026-05-27 10:38:30 +0200

    amelioration de la gestion des routes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
-import { computed } from 'vue'
 import { useRouter } from 'vue-router'
+import { useProducts } from '@/composables/useProducts'
 
 const router = useRouter()
-const currentRoute = computed(() => {
-  return router.currentRoute.value.path
-})
+
+const { input, products } = useProducts()
+
+function handleBack() {
+  if (router.currentRoute.value.path === '/search') {
+    input.value = ''
+    products.value = []
+  }
+  router.back()
+}
 </script>
 
 <template>
@@ -119,8 +126,8 @@ const currentRoute = computed(() => {
     </svg>
   </div>
   <header class="h-[172px]">
-    <div v-if="currentRoute !== '/'" id="back-link" class="flex items-center mb-4 cursor-pointer">
-      <a @click="$router.back()" class="flex">
+    <div v-if="router.currentRoute.value.path !== '/'" id="back-link" class="flex items-center mb-4 cursor-pointer">
+      <a @click="handleBack" class="flex">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
           <path
             d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
